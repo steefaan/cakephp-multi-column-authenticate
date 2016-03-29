c4930b0a4547f93b1de1b33b50f043d401e8ba14
Stefan D <stefan@php-engineer.de>
2016-03-30 00:47:53 +0200

    adjust bootstrap for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,17 @@ require_once 'vendor/autoload.php';
 
 require 'vendor/cakephp/cakephp/tests/bootstrap.php';
 
+if (!getenv('db_dsn')) {
+    putenv('db_dsn=sqlite:///:memory:');
+}
+if (!getenv('DB')) {
+    putenv('DB=sqlite');
+}
+
+ConnectionManager::config('test', [
+    'url' => getenv('db_dsn')
+]);
+
 Plugin::load('MultiColumnAuthenticate', [
     'path' => dirname(dirname(__FILE__)) . DS
 ]);
